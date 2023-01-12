addfe9e27ef11a05f34c683d81d9d2f32cd6182c
Stephan Mühl <31169771+Blueforcer@users.noreply.github.com>
2023-01-12 10:33:36 +0100

    Update PlayniteShell.bat

--- PlayniteShell.bat
+++ PlayniteShell.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
-START "" "C:\Program Files\VideoLAN\VLC\vlc.exe" --fullscreen --video-on-top --play-and-exit --no-video-title -Idummy \Videos\BootVideo.mp4
+START "" "C:\Program Files\VideoLAN\VLC\vlc.exe" --fullscreen --video-on-top --play-and-exit --no-video-title -Idummy %USERPROFILE%\Documents\PlayniteShell\Videos\BootVideo.mp4
 START "" "%LOCALAPPDATA%\Playnite\Playnite.FullscreenApp.exe" --hidesplashscreen
 TIMEOUT /T 3
-START "" "StartDesktop.lnk"
+START "" "%USERPROFILE%\Documents\PlayniteShell\StartDesktop.lnk"
 EXIT
